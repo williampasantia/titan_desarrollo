8dc8be8fba3d66bc6b0a257f7a82ea8d7168c769
Juan David Lara <jdavid.6700@gmail.com>
2016-01-15 22:47:39 -0500

    Carga Preliminar Bloque Concepto

---
+++ blocks/bloquesConcepto/contenidoConcepto/Sql.class.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace bloquesConcepto\contenidoConcepto;
+
+if (! isset ( $GLOBALS ["autorizado"] )) {
+    include ("../index.php");
+    exit ();
+}
+
+include_once ("core/manager/Configurador.class.php");
+include_once ("core/connection/Sql.class.php");
+
+/**
+ * IMPORTANTE: Se recomienda que no se borren registros. Utilizar mecanismos para - independiente del motor de bases de datos,
+ * poder realizar rollbacks gestionados por el aplicativo.
+ */
+
+
+
+class Sql extends \Sql {
+    
+    var $miConfigurador;
+    
+    function getCadenaSql($tipo, $variable = '') {
+        
+        
+        
+        /**
+         * 1.
+         * Revisar las variables para evitar SQL Injection
+         */
+        $prefijo = $this->miConfigurador->getVariableConfiguracion ( "prefijo" );
+        $idSesion = $this->miConfigurador->getVariableConfiguracion ( "id_sesion" );
+        $cadenaSql='';
+        switch ($tipo) {
+            
+            /**
+             * Clausulas específicas
+             */
+            case 'insertarRegistro' :
+                $cadenaSql = 'INSERT INTO ';
+                $cadenaSql .= 'parametro.cargo ';
+                $cadenaSql .= '( ';
+                $cadenaSql .= 'nivel,';
+                $cadenaSql .= 'codigo_alternativo,';
+                $cadenaSql .= 'grado,';
+                $cadenaSql .= 'nombre,';
+                $cadenaSql .= 'cod_tipo_cargo,';
+                $cadenaSql .= 'sueldo,';
+                $cadenaSql .= 'tipo_sueldo,';
+                $cadenaSql .= 'estado';
+                $cadenaSql .= ') ';
+                $cadenaSql .= 'VALUES ';
+                $cadenaSql .= '( ';
+                $cadenaSql .= $variable ['nivelRegistro'] . ', ';
+                $cadenaSql .= $variable ['codAlternativoRegistro'] . ', ';
+                $cadenaSql .= $variable ['gradoRegistro'] . ', ';
+                $cadenaSql .= '\'' . $variable ['nombreRegistro']  . '\', ';
+                $cadenaSql .= '\'' . $variable ['codTipoCargoRegistro'] . '\', ';
+                $cadenaSql .= $variable ['sueldoRegistro'] . ', ';
+                $cadenaSql .= '\'' . $variable['tipoSueldoRegistro'] . '\', ';
+                $cadenaSql .= '\'' . $variable['estadoRegistro'] . '\' ';
+                $cadenaSql .= ') ';
+                break;
+        
+        }
+        
+       
+        
+        return $cadenaSql;
+    
+    }
+}
+?>
