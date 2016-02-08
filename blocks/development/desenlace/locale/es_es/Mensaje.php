b0bb867369d1aee77bb3b7867f109c3ac122f52a
William <williamlinaresg2@gmail.com>
2016-02-08 05:15:44 -0500

    Correcion Archivos Repositorio

--- blocks/development/desenlace/locale/es_es/Mensaje.php
+++
@@ -1,7 +0,0 @@
-<?php
-$this->idioma ['formularioDesenlace'] = 'Ingresar la cadena a analizar.';
-$this->idioma ['campoCadena'] = 'Cadena: ';
-$this->idioma ['campoCadenaTitulo'] = 'Cadena a analizar';
-$this->idioma ['botonAceptar'] = 'Aceptar';
-$this->idioma ['noDefinido'] = 'Etiqueta No definida';
-?>
